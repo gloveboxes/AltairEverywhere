c2216b615c3f020c25f28e6818384e1edc3f73b6
Dave Glover <dglover@microsoft.com>
2022-01-01 22:03:41 +1100

    Initial check in

--- Altair_Deploy_OTA/list_deployed_images.ps1
+++
@@ -1,6 +0,0 @@
-./config.ps1
-
-
-Write-Host "`nListing images for device group id: $device_group from tenant id: $tenant_id`n"
-
-azsphere device-group deployment list --device-group $device_group -t $tenant_id -o json
--- Altair_Deploy_OTA/show_image_detail.ps1
+++
@@ -1,5 +0,0 @@
-./config.ps1
-
-$image_id = "REPLACE_WITH_YOUR_IMAGE_ID"
-
-azsphere image show -i $image_id -t $global:tenant_id
