8be64a60bbb2d141d8e11392c80b6d08f2a1f40b
Dave Glover <dglover@microsoft.com>
2022-02-08 20:33:14 +1100

    updated docker build and run

--- Docker/altair.sh
+++ Docker/altair.sh
@@ -2,9 +2,13 @@
 
 mosquitto -c /etc/mosquitto/mosquitto.conf &
 
-cd /Altair8800Linux/AltairHL_emulator
+cd /Altair8800.Emulator.UN-X/
 
-./build/Altair_emulator "-s" "0ne000F25AF" "-d" "desktopdevx" "-k" "Mnk4T7wevKznmYcrjEGCHnlWSFDvv+/uUf6uIttPEmY=" 
+cd Terminal && python3 -m http.server 8081 &
+
+cd /Altair8800.Emulator.UN-X/AltairHL_emulator
+
+./build/Altair_emulator "-s" "0ne000F25AF" "-d" "docker" "-k" "zXE/bDrlOp0YzlITcD8oLSCI8NS0UXVeND6fTUWSS/g=" -o "41cfa0029da8bd93cafdf1ee18aede4a" &
 
 
 wait -n
