94588bb3c2a52d57be1e5259322d85c9f7b4e29e
Dave Glover <dglover@microsoft.com>
2022-03-06 18:26:29 +1100

    version bump

--- AltairHL_emulator/main.h
+++ AltairHL_emulator/main.h
@@ -43,7 +43,7 @@
 #include "front_panel_none.h"
 #endif // ALTAIR_FRONT_PANEL_PI_SENSE
 
-#define ALTAIR_ON_AZURE_SPHERE_VERSION "4.0.7"
+#define ALTAIR_ON_AZURE_SPHERE_VERSION "4.0.8"
 #define Log_Debug(f_, ...) dx_Log_Debug((f_), ##__VA_ARGS__)
 #define DX_LOGGING_ENABLED FALSE
 
