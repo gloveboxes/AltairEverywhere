da61216117ea0b3a8545dbc090b09caab943ffdc
Dave Glover <dglover@microsoft.com>
2022-03-08 17:36:53 +1100

    Updated docker support

--- Docker/altair.sh
+++ Docker/altair.sh
@@ -6,7 +6,7 @@ cd /Altair8800/Terminal && python3 -m http.server 8081 &
 
 cd /Altair8800/AltairHL_emulator
 
-./build/Altair_emulator -s $ID_SCOPE -d $DEVICE_ID -k $DERIVED_KEY -n "eth0" -o $OPEN_WEATHER_MAP_API_KEY -a $AIR_VISUAL_API_KEY &
+./build/Altair_emulator -s $ID_SCOPE -d $DEVICE_ID -k $DERIVED_KEY -n "eth0" -o $OPEN_WEATHER_MAP_API_KEY &
 
 wait -n
 
