abad658aa41b8287427ba29912a0234674e4826d
Dave Glover <dglover@microsoft.com>
2022-04-20 13:53:18 +1000

    version bump and publish delay 2 secs

--- AltairHL_emulator/io_ports.c
+++ AltairHL_emulator/io_ports.c
@@ -190,8 +190,8 @@ void io_port_out(uint8_t port, uint8_t data)
 				{
 					ju.publish_pending = true;
 					ju.index           = 0;
-					// Throttle IoT messages to 1 second as IoT dowork clocked at 500ms
-					dx_timerOneShotSet(&tmr_deferred_port_out_json, &(struct timespec){1, 0});
+					// Throttle IoT messages to 2 second as IoT dowork clocked at 500ms
+					dx_timerOneShotSet(&tmr_deferred_port_out_json, &(struct timespec){2, 0});
 				}
 			}
 			break;
@@ -200,8 +200,8 @@ void io_port_out(uint8_t port, uint8_t data)
 			{
 				publish_weather_pending = true;
 				jitter                  = (int)data;
-				// Throttle IoT messages to 1 second as IoT dowork clocked at 500ms
-				dx_timerOneShotSet(&tmr_deferred_port_out_weather, &(struct timespec){1, 0});
+				// Throttle IoT messages to 2 second as IoT dowork clocked at 500ms
+				dx_timerOneShotSet(&tmr_deferred_port_out_weather, &(struct timespec){2, 0});
 			}
 			break;
 		case 33: // copy file from web server to mutable storage
