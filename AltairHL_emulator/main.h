62c49139f86e62e0aeac3931ad85b81577b01b6e
Dave Glover <dglover@microsoft.com>
2022-05-09 17:48:00 +1000

    Updated DevX Async Binding

--- AltairHL_emulator/main.h
+++ AltairHL_emulator/main.h
@@ -40,7 +40,7 @@
 #include "front_panel_none.h"
 #endif // ALTAIR_FRONT_PANEL_PI_SENSE
 
-#define ALTAIR_EMULATOR_VERSION "4.5.2"
+#define ALTAIR_EMULATOR_VERSION "4.5.3"
 #define Log_Debug(f_, ...)      dx_Log_Debug((f_), ##__VA_ARGS__)
 #define DX_LOGGING_ENABLED      FALSE
 
@@ -120,13 +120,13 @@ static DX_TIMER_BINDING tmr_report_memory_usage = {.repeat = &(struct timespec){
 static DX_TIMER_BINDING tmr_tick_count = {.repeat = &(struct timespec){1, 0}, .name = "tmr_tick_count", .handler = tick_count_handler};
 static DX_TIMER_BINDING tmr_update_environment = {.delay = &(struct timespec){2, 0}, .name = "tmr_update_environment", .handler = update_environment_handler};
 
-DX_ASYNC_BINDING async_copyx_request = {.handler = async_copyx_request_handler};
-DX_ASYNC_BINDING async_deferred_command = {.handler = async_deferred_command_handler};
-DX_ASYNC_BINDING async_expire_session = {.handler = async_expire_session_handler};
-DX_ASYNC_BINDING async_publish_json = {.handler = async_publish_json_handler};
-DX_ASYNC_BINDING async_publish_weather = {.handler = async_publish_weather_handler};
-DX_ASYNC_BINDING async_set_timer = {.handler = async_set_timer_handler};
-DX_ASYNC_BINDING async_terminal = {.handler = async_terminal_handler};
+DX_ASYNC_BINDING async_copyx_request = {.name = "async_copyx_request", .handler = async_copyx_request_handler};
+DX_ASYNC_BINDING async_deferred_command = {.name = "async_deferred_command", .handler = async_deferred_command_handler};
+DX_ASYNC_BINDING async_expire_session = { .name = "async_expire_session", .handler = async_expire_session_handler};
+DX_ASYNC_BINDING async_publish_json = {.name = "async_publish_json", .handler = async_publish_json_handler};
+DX_ASYNC_BINDING async_publish_weather = {.name = "async_publish_weather", .handler = async_publish_weather_handler};
+DX_ASYNC_BINDING async_set_timer = {.name = "async_set_timer", .handler = async_set_timer_handler};
+DX_ASYNC_BINDING async_terminal = {.name = "async_terminal", .handler = async_terminal_handler};
 
 // Azure IoT Central Properties (Device Twins)
 
