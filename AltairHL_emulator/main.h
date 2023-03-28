0894175bdb164d1e3f58f41c13d7befd0d345444
Dave Glover <dglover@microsoft.com>
2023-03-28 18:52:59 +1100

    altair version bump

--- AltairHL_emulator/main.h
+++ AltairHL_emulator/main.h
@@ -49,7 +49,7 @@
 #include "front_panel_none.h"
 #endif
 
-const char ALTAIR_EMULATOR_VERSION[] = "4.7.8";
+const char ALTAIR_EMULATOR_VERSION[] = "4.7.9";
 #define Log_Debug(f_, ...) dx_Log_Debug((f_), ##__VA_ARGS__)
 #define DX_LOGGING_ENABLED FALSE
 
