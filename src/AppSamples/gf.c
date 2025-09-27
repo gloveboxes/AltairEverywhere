3afa14e452f0992a10706c28fedca13a9d169036
Dave Glover <dglover@microsoft.com>
2025-09-27 14:58:48 +1000

    update terminal gf dx libs and sub

--- src/AppSamples/gf.c
+++ src/AppSamples/gf.c
@@ -7,32 +7,14 @@ int get_endpoint();
 int get_filename();
 int set_selected();
 int save_ep_url();
-int set_stg_fname();
-int immutable_copy();
-int web_copy();
-int set_fname_web();
+int dxstgfn();
+int dxstgcpy();
+int dxwebcpy();
+int dxwebfn();
+int dxseturl();
+int dxgeturl();
 int validate_endpoint();
 int str_tolower();
-
-#define STG_FILENAME 68
-#define STG_EOF      68
-#define STG_GET_BYTE 202
-
-#define WG_IDX_RESET 109
-#define WG_EP_NAME   110
-#define WG_GET_URL  111
-#define WWG_SET_URL 112
-#define WG_SELECTED 113
-#define WG_FILENAME 114
-#define WG_GET_BYTE 201
-#define WG_STATUS   33
-
-#define WG_EOF       0
-#define WG_WAITING   1
-#define WG_DATAREADY 2
-#define WG_FAILED    3
-
-#define GET_STRING 200
 
 FILE *fp_input;
 FILE *fp_output;
@@ -45,7 +27,7 @@ char file_content[128];
 int defaults()
 {
     FILE *fp;
-    int len, c;
+    int len;
 
     /* Load endpoint from gf.txt */
     fp = fopen("gf.txt", "r");
@@ -63,13 +45,7 @@ int defaults()
                     len--;
                 }
 
-                outp(WG_IDX_RESET, 0);
-                endpoint = file_content;
-                for (c = 0; c < len; c++)
-                {
-                    outp(WG_EP_NAME, endpoint[c]);
-                }
-                outp(WG_EP_NAME, 0);
+                dxseturl(file_content, len);
             }
         }
         fclose(fp);
@@ -82,16 +58,17 @@ int main(argc, argv)
 int argc;
 char **argv;
 {
-    int source, wg_result;
+    int wg_result, bytes_written;
 
     wg_result = 0;
+    bytes_written = 0;
     fp_input = fopen("stdin", "r");
 
     defaults();
 
     printf("\nGF (Get File) - File Transfer Utility v%s\n", GF_VERSION);
     printf("Transfer files from Azure Sphere storage or web over HTTP(s)\n");
-    printf("Available flags: --help, --version, -e <url>\n\n");
+    printf("Available flags: --help, --version, -e <url>, -f <filename>\n\n");
 
     if (argc == 3)
     {
@@ -111,10 +88,53 @@ char **argv;
                 return -1;
             }
         }
+        else if (strcmp(argv[1], "-f") == 0 || strcmp(argv[1], "-F") == 0)
+        {
+            filename = argv[2];
+
+            if (strlen(file_content) == 0)
+            {
+                printf("Error: No endpoint URL found in gf.txt. Use -e to set an endpoint first.\n");
+                return -1;
+            }
+
+
+
+            printf("\nDownloading file '%s' from URL '%s'\n", filename, file_content);
+
+            if ((fp_output = fopen(filename, "w")) == NULL)
+            {
+                printf("Error: Failed to create output file '%s'\n", filename);
+                printf("Check disk space and write permissions.\n");
+                return -1;
+            }
+
+            /* Set selected_ep to the default endpoint (3rd, starting from zero) */
+            selected_ep = 2; /* Default to the third endpoint */
+
+            dxwebfn(filename, strlen(filename), selected_ep);
+            wg_result = dxwebcpy(fp_output, &bytes_written);
+            if (wg_result == 0)
+            {
+                printf(" done (%d bytes)\n", bytes_written);
+            }
+            else
+            {
+                printf(" failed\n");
+            }
+
+            fclose(fp_output);
+            if (wg_result == -1)
+            {
+                printf("\n\nWeb copy failed for file '%s'. Check filename and network connection\n", filename);
+                unlink(filename);
+            }
+            return 0;
+        }
         else
         {
             printf("Unrecognized option: %s\n", argv[1]);
-            printf("Usage: gf [--help] [-e <url>]\n");
+            printf("Usage: gf [--help] [-e <url>] [-f <filename>]\n");
             return -1;
         }
     }
@@ -166,7 +186,7 @@ char **argv;
         {
         }
 
-        printf("\nTransferring file '%s' from endpoint '%d'\n\n", filename, selected_ep);
+        printf("\nTransferring file '%s' from endpoint '%d'\n\n", filename, selected_ep + 1);
     }
 
     if ((fp_output = fopen(filename, "w")) == NULL)
@@ -179,16 +199,42 @@ char **argv;
     switch (selected_ep)
     {
         case 0:
-            set_stg_fname(filename);
-            immutable_copy();
+            printf("Copying from storage...");
+            dxstgfn(filename);
+            if (dxstgcpy(fp_output, &bytes_written) == 0)
+            {
+                printf(" done (%d bytes)\n", bytes_written);
+            }
+            else
+            {
+                printf(" failed\n");
+            }
             break;
         case 1:
-            set_fname_web(filename, strlen(filename), 0);
-            wg_result = web_copy();
+            printf("Downloading from web endpoint %d...", selected_ep + 1);
+            dxwebfn(filename, strlen(filename), 0);
+            wg_result = dxwebcpy(fp_output, &bytes_written);
+            if (wg_result == 0)
+            {
+                printf(" done (%d bytes)\n", bytes_written);
+            }
+            else
+            {
+                printf(" failed\n");
+            }
             break;
         case 2:
-            set_fname_web(filename, strlen(filename), 1);
-            wg_result = web_copy();
+            printf("Downloading from web endpoint %d...", selected_ep + 1);
+            dxwebfn(filename, strlen(filename), 1);
+            wg_result = dxwebcpy(fp_output, &bytes_written);
+            if (wg_result == 0)
+            {
+                printf(" done (%d bytes)\n", bytes_written);
+            }
+            else
+            {
+                printf(" failed\n");
+            }
             break;
         default:
             break;
@@ -203,178 +249,83 @@ char **argv;
     return 0;
 }
 
+int str_tolower(str)
+char *str;
+{
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = str[i] + 32;
+        }
+    }
+    return 0;
+}
+
+int validate_endpoint(url)
+char *url;
+{
+    char check_url[6];
+    int c;
+    int len;
+
+    if (url == NULL)
+    {
+        return -1;
+    }
+
+    len = 0;
+    while (url[len] != '\0')
+    {
+        len++;
+    }
+
+    if (len < 8 || len > 120)
+    {
+        return -1;
+    }
+
+    /* Check for http:// or https:// */
+    for (c = 0; c < 4 && c < len; c++)
+    {
+        check_url[c] = (url[c] >= 'a' && url[c] <= 'z') ? url[c] - 32 : url[c];
+    }
+    check_url[4] = '\0';
+
+    if (check_url[0] == 'H' && check_url[1] == 'T' && check_url[2] == 'T' && check_url[3] == 'P')
+    {
+        /* Verify it has :// after http */
+        if (len > 6 && url[4] == ':' && url[5] == '/' && url[6] == '/')
+        {
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
 int save_ep_url(endpoint)
 char *endpoint;
 {
-    int len, c;
     FILE *fp;
 
     /* write endpoint to gf.txt */
     fp = fopen("gf.txt", "w");
     if (fp != NULL)
     {
-        fprintf(fp, "%s\n", endpoint);
+        int i;
+        i = 0;
+        while (endpoint[i] != '\0')
+        {
+            fputc(endpoint[i], fp);
+            i++;
+        }
+        fputc('\n', fp);
         fclose(fp);
     }
 
-    return 0;
-}
-
-/* get personal endpoint url */
-int get_ep_url(endpoint)
-int endpoint;
-{
-    char c;
-    printf("%d) ", endpoint + 1);
-    outp(WG_GET_URL, 0);
-    while ((c = inp(GET_STRING)) != 0)
-    {
-        printf("%c", c);
-    }
-    printf("%c", '\n');
-    return 0;
-}
-
-/* Get selected endpoint URL */
-int get_selected_ep()
-{
-    char c;
-    printf("Selected endpoint: ");
-    outp(WG_SELECTED, 0);
-    while ((c = inp(GET_STRING)) != 0)
-    {
-        printf("%c", c);
-    }
-    printf("%c", '\n');
-    return 0;
-}
-
-/* Sets the filename to be copied with i8080 port 68 */
-int set_stg_fname(filename)
-char *filename;
-{
-    int c;
-    int len;
-
-    len = strlen(filename);
-
-    for (c = 0; c < len; c++)
-    {
-        outp(STG_FILENAME, filename[c]);
-    }
-    outp(STG_FILENAME, 0);
-    printf("filename set\n");
-    return 0;
-}
-
-/* copies file byte stream from i8080 port 202 */
-int immutable_copy()
-{
-    char ch;
-    int byte_count;
-
-    byte_count = 0;
-    printf("Copying from storage");
-    
-    /* While not end of file read in next byte */
-    while ((ch = inp(STG_EOF)) == 0)
-    {
-        fputc(inp(STG_GET_BYTE), fp_output);
-        byte_count++;
-        if ((byte_count % 50) == 0)
-        {
-            printf(".");
-        }
-    }
-    printf("\nTransfer complete: %d bytes copied\n", byte_count);
-    return 0;
-}
-
-/* set the selected endpoint url */
-int set_selected()
-{
-    char *endpoint;
-
-    printf("Select endpoint:\n");
-    printf("1) Azure Sphere immutable storage\n");
-    printf("2) https://raw.githubusercontent.com/AzureSphereCloudEnabledAltair8800/RetroGames/main\n");
-    get_ep_url(2);
-
-    printf("\nSelect endpoint number: ");
-    endpoint = fgets(buf, 128, fp_input);
-
-    if (strlen(endpoint) > 2)
-    {
-        return -1;
-    }
-
-    if (endpoint[0] >= '1' && endpoint[0] <= '3')
-    {
-        selected_ep = atoi(endpoint) - 1;
-        return 0;
-    }
-    return -1;
-}
-
-/* Sets the filename to be copied with i8080 port 68 */
-int set_fname_web(filename, len, endpoint)
-char *filename;
-int len;
-int endpoint;
-{
-    int c;
-    
-    /* Set web endpoint */
-    outp(WWG_SET_URL, endpoint);
-    outp(WG_IDX_RESET,0);
-
-    for (c = 0; c < len; c++)
-    {
-        outp(WG_FILENAME, filename[c]);
-    }
-    outp(WG_FILENAME, 0);
-    return 0;
-}
-
-/* copies file byte stream */
-int web_copy()
-{
-    char status;
-    int byte_count, dot_count;
-
-    byte_count = 0;
-    dot_count = 0;
-    printf("Downloading from web");
-
-    while ((status = inp(WG_STATUS)) != WG_EOF)
-    {
-        if (status == WG_FAILED)
-        {
-            printf("\nWeb transfer failed\n");
-            return -1;
-        }
-
-        if (status == WG_DATAREADY)
-        {
-            fputc(inp(WG_GET_BYTE), fp_output);
-            byte_count++;
-            if ((byte_count % 50) == 0)
-            {
-                printf(".");
-                dot_count++;
-                if ((dot_count % 40) == 0)
-                {
-                    printf("\n");
-                }
-            }
-        }
-        else if (status == WG_WAITING)
-        {
-            printf("*");
-        }
-    }
-
-    printf("\nDownload complete: %d bytes received\n", byte_count);
     return 0;
 }
 
@@ -391,7 +342,11 @@ int get_filename()
         return -1;
     }
 
-    len = strlen(filename);
+    len = 0;
+    while (filename[len] != '\0')
+    {
+        len++;
+    }
 
     if (len < 2 || len > 13)
     {
@@ -426,8 +381,13 @@ int get_endpoint()
 
     endpoint = fgets(buf, 128, fp_input);
 
-    len = strlen(endpoint);
+    len = 0;
+    while (endpoint[len] != '\0')
+    {
+        len++;
+    }
     len--;
+
     endpoint[len] = 0x00;
 
     if (len < 8)
@@ -451,55 +411,34 @@ int get_endpoint()
     return 0;
 }
 
-int validate_endpoint(url)
-char *url;
+int set_selected()
 {
-    char check_url[6];
-    int c;
-    int len;
+    char *endpoint;
+    char url_buffer[128];
+    int url_len;
 
-    if (url == NULL)
+    printf("Select endpoint:\n");
+    printf("1) Azure Sphere immutable storage\n");
+    printf("2) https://raw.githubusercontent.com/AzureSphereCloudEnabledAltair8800/RetroGames/main\n");
+
+    url_len = dxgeturl(2, url_buffer, 128);
+    if (url_len > 0)
+    {
+        printf("3) %s\n", url_buffer);
+    }
+
+    printf("\nSelect endpoint number: ");
+    endpoint = fgets(buf, 128, fp_input);
+
+    if (strlen(endpoint) > 2)
     {
         return -1;
     }
 
-    len = strlen(url);
-
-    if (len < 8 || len > 120)
+    if (endpoint[0] >= '1' && endpoint[0] <= '3')
     {
-        return -1;
+        selected_ep = atoi(endpoint) - 1;
+        return 0;
     }
-
-    /* Check for http:// or https:// */
-    for (c = 0; c < 4 && c < len; c++)
-    {
-        check_url[c] = toupper(url[c]);
-    }
-    check_url[4] = 0x00;
-
-    if (strcmp(check_url, "HTTP") == 0)
-    {
-        /* Verify it has :// after http */
-        if (len > 6 && url[4] == ':' && url[5] == '/' && url[6] == '/')
-        {
-            return 0;
-        }
-    }
-
     return -1;
-}
-
-int str_tolower(str)
-char *str;
-{
-    int i;
-    
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] >= 'A' && str[i] <= 'Z')
-        {
-            str[i] = str[i] + 32;
-        }
-    }
-    return 0;
 }
